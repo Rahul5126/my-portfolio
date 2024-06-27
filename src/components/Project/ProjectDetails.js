11d9cd3c79cc1c27ede352adf0c042b6296a9a9a
Rahul Singh <rahulofficial5126@gmail.com>
2024-06-28 00:11:13 +0530

    links updated

--- src/components/Project/ProjectDetails.js
+++ src/components/Project/ProjectDetails.js
@@ -1,112 +1,75 @@
 export const ProjectDetails = [
     {
-      title: "Business Startup",
-      description: "Design & Development. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.",
-      imgUrl: require('../../assets/img/project-img1.png'),
+      title: "Music Website",
+      description: "This project is a dynamic web application focused on music discovery, featuring curated playlists, top artists, and trending songs across various genres. Users can explore, search, and play music, enjoying personalized music experiences",
+      imgUrl: require('../../assets/img/project-img5.jpeg'),
       category: "Javascript",
-      techUsed : ["React", "Node.js", "Express"],
-      liveDemo : "https://rahulthedeveloper.netlify.app",
-      githubCode : "https://github.com/Rahul5126/personal-portfolio"
+      techUsed : ["HTML5", "CSS3", "Javascript"],
+      liveDemo : "https://melody-lane.netlify.app/",
+      githubCode : "https://github.com/Rahul5126/music-website"
     },
     {
-      title: "Business Startup",
-      description: "Design & Development. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.",
-      imgUrl: require('../../assets/img/project-img1.png'),
+      title: "The Simon Game",
+      description: "The Simon game is an interactive memory challenge built using HTML, CSS, and JavaScript. Players must follow a sequence of colored buttons that light up and play corresponding sounds. This project showcases my proficiency in creating dynamic and engaging web games.",
+      imgUrl: require('../../assets/img/project-img4.jpeg'),
       category: "Javascript",
-      techUsed : ["React", "Node.js", "Express"],
-      liveDemo : "https://rahulthedeveloper.netlify.app",
-      githubCode : "https://github.com/Rahul5126/personal-portfolio"
+      techUsed : ["HTML5", "CSS3", "Javascript"],
+      liveDemo : "https://thesimon-game.netlify.app/",
+      githubCode : "https://github.com/Rahul5126/the-simon-game"
     },
     {
-      title: "Business Startup",
-      description: "Design & Development. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.",
-      imgUrl: require('../../assets/img/project-img1.png'),
+      title: "Color Switcher",
+      description: "This project features a dynamic website that allows users to customize the color theme, built using HTML, CSS, and JavaScript. Users can switch between different color schemes and toggle between light and dark modes. The project showcases my ability to create interactive and user-friendly web interfaces.",
+      imgUrl: require('../../assets/img/project-img3.jpeg'),
       category: "Javascript",
-      techUsed : ["React", "Node.js", "Express"],
-      liveDemo : "https://rahulthedeveloper.netlify.app",
-      githubCode : "https://github.com/Rahul5126/personal-portfolio"
+      techUsed : ["HTML5", "CSS3", "Javascript"],
+      liveDemo : "https://switch-color.netlify.app/",
+      githubCode : "https://github.com/Rahul5126/color-switcher"
     },
     {
-      title: "Business Startup",
-      description: "Design & Development. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.",
-      imgUrl: require('../../assets/img/project-img1.png'),
+      title: "Roll the Dice",
+      description: "The Dicee project is a simple and interactive dice game for two players, built using HTML, CSS, and JavaScript. Players can roll the dice to see who wins, with results dynamically displayed on the screen. This project highlights my skills in creating fun and engaging web applications.",
+      imgUrl: require('../../assets/img/project-img2.jpeg'),
       category: "Javascript",
-      techUsed : ["React", "Node.js", "Express"],
-      liveDemo : "https://rahulthedeveloper.netlify.app",
-      githubCode : "https://github.com/Rahul5126/personal-portfolio"
+      techUsed : ["HTML5", "CSS3", "Javascript"],
+      liveDemo : "",
+      githubCode : "https://github.com/Rahul5126/roll-the-dice"
     },
     {
-      title: "Business Startup",
-      description: "Design & Development. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.",
-      imgUrl: require('../../assets/img/project-img1.png'),
+      title: "Drum Kit",
+      description: "The Drum Kit project is an interactive web application that allows users to play drum sounds by clicking on buttons or pressing corresponding keys on their keyboard. This project showcases my skills in HTML, CSS, and JavaScript, and it provides an engaging and fun user experience.",
+      imgUrl: require('../../assets/img/project-img1.jpeg'),
       category: "Javascript",
-      techUsed : ["React", "Node.js", "Express"],
-      liveDemo : "https://rahulthedeveloper.netlify.app",
-      githubCode : "https://github.com/Rahul5126/personal-portfolio"
+      techUsed : ["HTML5", "CSS3", "Javascript"],
+      liveDemo : "https://beat-drum-kit.netlify.app/",
+      githubCode : "https://github.com/Rahul5126/drum-kit"
     },
     {
-      title: "Business Startup",
-      description: "Design & Development. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.",
-      imgUrl: require('../../assets/img/project-img1.png'),
-      category: "Javascript",
-      techUsed : ["React", "Node.js", "Express"],
-      liveDemo : "https://rahulthedeveloper.netlify.app",
-      githubCode : "https://github.com/Rahul5126/personal-portfolio"
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.",
-      imgUrl: require('../../assets/img/project-img1.png'),
-      category: "Javascript",
-      techUsed : ["React", "Node.js", "Express"],
-      liveDemo : "https://rahulthedeveloper.netlify.app",
-      githubCode : "https://github.com/Rahul5126/personal-portfolio"
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.",
-      imgUrl: require('../../assets/img/project-img1.png'),
-      category: "Javascript",
-      techUsed : ["React", "Node.js", "Express"],
-      liveDemo : "https://rahulthedeveloper.netlify.app",
-      githubCode : "https://github.com/Rahul5126/personal-portfolio"
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.",
-      imgUrl: require('../../assets/img/project-img1.png'),
-      category: "Javascript",
-      techUsed : ["React", "Node.js", "Express"],
-      liveDemo : "https://rahulthedeveloper.netlify.app",
-      githubCode : "https://github.com/Rahul5126/personal-portfolio"
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.",
-      imgUrl: require('../../assets/img/project-img1.png'),
-      category: "Javascript",
-      techUsed : ["React", "Node.js", "Express"],
-      liveDemo : "https://rahulthedeveloper.netlify.app",
-      githubCode : "https://github.com/Rahul5126/personal-portfolio"
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: require('../../assets/img/project-img2.png'),
+      title: "My Portfolio",
+      description: "This React-based portfolio showcases my expertise in web development, featuring interactive project components with Live demos and GitHub links. The design leverages CSS3 and Bootstrap for a responsive and visually appealing user experience. It includes a comprehensive 'About Me' section, project showcases, and contact form to engage potential collaborators",
+      imgUrl: require('../../assets/img/project-img7.jpeg'),
       category: "React",
-      techUsed : ["React", "Node.js", "Express"],
-      liveDemo : "https://rahulthedeveloper.netlify.app",
-      githubCode : "https://github.com/Rahul5126/personal-portfolio"
+      techUsed : ["React", "CSS3", "Bootstrap"],
+      liveDemo : "https://rahulthedeveloper.netlify.app/",
+      githubCode : "https://github.com/Rahul5126/my-portfolio"
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: require('../../assets/img/project-img3.png'),
-      category: "MERN",
-      techUsed : ["React", "Node.js", "Express"],
-      liveDemo : "https://rahulthedeveloper.netlify.app",
-      githubCode : "https://github.com/Rahul5126/personal-portfolio"
+      title: "Weather Web",
+      description: "Applied React.js framework to build a dynamic and responsive user interface. Integrated the OpenWeatherMap API to fetch real-time weather data. Employed modular design with components such as Search, Time, Location, Temperature and Details for efficient organization and display of weather information.",
+      imgUrl: require('../../assets/img/project-img6.jpeg'),
+      category: "React",
+      techUsed : ["React", "Bootstrap"],
+      liveDemo : "https://instant-weather-forcast.netlify.app/",
+      githubCode : "https://github.com/Rahul5126/weather-web"
     },
-    // Add more projects as needed
+    {
+      title: "E-Commerce",
+      description: "Utilized a comprehensive full-stack development approach using technologies such as React.js, Node.js, Express.js, and MongoDB. Implemented key features, including user authentication, product management, and shopping cart functionality. The admin dashboard offers a centralized platform for managing the product inventory, enabling efficient management of product details, images, and categories.",
+      imgUrl: require('../../assets/img/project-img8.jpeg'),
+      category: "MERN",
+      techUsed : ["React", "CSS3", "Node", "Express", "Mongodb"],
+      liveDemo : "",
+      githubCode : "https://github.com/Rahul5126/MERN-E-COMMERCE"
+    },
 ];
   
